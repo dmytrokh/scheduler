26b5e9f17f380e778d9f5ae303651ee7b43a903e
dmitrokh <dmitry.kharchenko@gmail.com>
2015-09-23 12:23:19 +0300

    add open browser

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var morgan = require('morgan');
+var open = require("open");
+
 var app = express();
 
 app.use(morgan('dev'));
 app.use(express.static("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
 
+open("http://localhost:5000/");
